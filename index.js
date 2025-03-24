747605f3456c1e6d995c1e6996a7720943c2f467
Arstan87 <arstan.kurmashev@mail.ru>
2025-03-24 18:17:25 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,60 +1,25 @@
-const { Telegraf, Markup } = require("telegraf");
-const { message } = require("telegraf/filters");
-require("dotenv").config();
-const text = require(`./const`);
+require('dotenv').config();
+const TelegramBot = require('node-telegram-bot-api');
+const express = require('express');
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
-bot.start(
-  (ctx) =>
-    console.log(ctx.message) &
-    ctx.reply(
-      `Привет ${
-        ctx.message.from.first_name ? ctx.message.from.first_name : "незнакомец"
-      }!`
-    )
-);
-bot.help((ctx) => ctx.reply(text.commands));
-bot.command("course", async (ctx) => {
-  try {
-    await ctx.replyWithHTML(
-      "<b>Курсы</b>",
-      Markup.inlineKeyboard([
-        [
-          Markup.button.callback("Редакторы", "btn_1"),
-          Markup.button.callback("Обзоры", "btn_2"),
-          Markup.button.callback("Редакторы 2", "btn_3"),
-        ],
-        [Markup.button.callback("HTML", "btn_4")],
-      ])
-    );
-  } catch (e) {
-    console.error(e);
-  }
+const token = process.env.TELEGRAM_BOT_TOKEN;
+const port = process.env.PORT || 3000;
+
+// Инициализация бота
+const bot = new TelegramBot(token, {polling: true});
+
+// Обработчик сообщений
+bot.on('message', (msg) => {
+  bot.sendMessage(msg.chat.id, "Бот работает на Railway!");
 });
 
-function addActionBot(name, src, text) {
-  bot.action(name, async (ctx) => {
-    try {
-      await ctx.answerCbQuery();
-      if (src !== false) {
-        await ctx.replyWithPhoto({
-          source: src,
-        });
-      }
-      await ctx.replyWithHTML(text, {
-        disable_web_page_preview: true,
-      });
-    } catch (e) {
-      console.error(e);
-    }
-  });
-}
-addActionBot("btn_1", "./img/1.jpg", text.text1);
-addActionBot("btn_2", "./img/2.jpg", text.text2);
-addActionBot("btn_3", false, text.text3);
+// Веб-сервер для проверки работы
+const app = express();
+app.get('/', (req, res) => {
+  res.send('PM87 Bot is running!');
+});
 
-bot.launch();
-
-// Enable graceful stop
-process.once("SIGINT", () => bot.stop("SIGINT"));
-process.once("SIGTERM", () => bot.stop("SIGTERM"));
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+console.log('Bot started at', new Date());
